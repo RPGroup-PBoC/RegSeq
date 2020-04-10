99ea6e60dbf30bf217ddff4b4910f7dee5369e4a
Tom Roeschinger <troeschi@caltech.edu>
2020-04-10 13:43:43 -0700

    new versions of notebooksa and additions to RegSeq.utils.py

--- RegSeq/utils.py
+++ RegSeq/utils.py
@@ -1,7 +1,12 @@
 import pymc
+import pandas as pd
 import scipy as sp
 import numpy as np
-import 
+import Bio.Seq as Seq
+import mpathic.simulate_library as simulate_library
+import mpathic.utils
+import mpathic.profile_mut as profile_mut
+import mpathic.profile_freq as profile_freq
 
 def choose_dict(dicttype,modeltype='LinearEmat'):
     '''Get numbering dictionary for either dna,rna, or proteins'''
@@ -149,7 +154,7 @@ def seq2array_for_matmodel(dataset_df, chunksize=1000):
         max_base = seq_dict_inv[max_index]
         wt_mat[i*4 + max_index] = 1
 
-   return scipy.sparse.csr(temp_mat),wt_mat
+    return scipy.sparse.csr(temp_mat),wt_mat
         
     
 
@@ -214,3 +219,198 @@ def dataset2mutarray(dataset_df, modeltype, chunksize=1000, rowsforwtcalc=100):
 
     # Return vararray as well as binary representation of wt seq
     return mutarray_csr, wtrow
+
+
+def find_seq(s, genome):
+    '''
+    Find sequence 115 bp upstream and 45 bp downstream of TSS. If gene is reverse 
+    transcribed, return reverse complement of sequence.
+    
+    Parameters
+    ----------
+    s : array-like
+        Name, TSS and transcription direction of a gene
+    genome : string
+        Reference genome
+        
+    Returns
+    -------
+    output : string
+        Genetic sequence 115 bp upstream and 45 bp downstream of TSS
+    '''
+    ss = int(s[1])
+    if s[2] == 'rev':
+        
+        gene = genome[ss-45:ss+115]
+        tempgene = Seq.Seq(gene)
+        outgene = str(tempgene.reverse_complement())
+    elif s[2] == 'fwd':
+        outgene = genome[ss-115:ss+45]
+        
+    return outgene
+
+
+def mut_seq_random(s, fwdprimer, revprimer, nseqs):
+    """Mutate sequence at given rate and add primers.
+    
+    Parameters
+    ----------
+    s : array-like
+        Name, TSS and transcription direction of a gene
+    fwdprimer : string
+        Forward primer
+    revprimer : string
+        Reverse primer
+    nseqs : Int
+        Number of generated sequences
+        
+    Returns
+    -------
+    outdf : Pandas.DataFrame
+        Dataframe of mutated sequences
+    """
+    
+    # mutate interal 140 bp.
+    firstbase = 10
+    finalbase = 150
+    
+    # use mpathic function to generate randomly mutated library
+    seqs_df = simulate_library.main(wtseq=s[firstbase:finalbase], numseq=nseqs)
+    # only take independent sequences
+    seqs_df = mpathic.utils.collapse_further(seqs_df)
+    seqs = seqs_df['seq']
+    # remove wild type sequences
+    outseqs = [fwdprimer + s + revprimer]  + [fwdprimer + s[:firstbase] + str(x) + s[finalbase:] + revprimer  for x in seqs]
+    outdf = pd.DataFrame()
+    outdf['seq'] = outseqs
+    return outdf
+
+
+def get_primers(fwd, rev, start, end):
+    """Return primers from Bio.Seq iterator objects.
+    
+    Parameters
+    ----------
+    fwd : Bio.Seq iterator
+        Library for forward primers
+    rev : Bio.Seq iterator
+        Library for reverse primers
+    start : Int
+        Index of first primers to be read
+    end : Int
+        Index of last primers to be read
+        
+    Returns
+    -------
+    fwd_list : List
+        List of forward primers
+    rev_list : List
+        List of reverse primers
+    """
+    
+    
+    fwd_list = []
+    rev_list = []
+    records = list(enumerate(zip(fwd, rev)))[start:end+1]
+    for i,(fwd_rec, rev_rec) in records:
+        fwd_prime = str(fwd_rec.seq)
+        rev_prime = str(rev_rec.seq)
+        fwd_list.append(fwd_prime)
+        rev_list.append(rev_prime)
+        
+    return fwd_list, rev_list
+        
+    
+    
+def mutation_sequences(genes, fwdprimes, revprimes, nseqs):
+    """
+    Get mutated sequences for a set of genes and primers.
+    
+    Parameters
+    ----------
+    genes : Pandas Dataframe
+        Gene names and sequences
+    fwdprimes : array-like
+        List of forward primers
+    revprimes : array-like
+        List of reverse primers
+    nseqs : int
+        Number of mutated sequences
+        
+    Returns
+    -------
+    allseqs : Pandas Dataframe
+        Dataframe of mutated sequences
+    """
+    
+    allseqs = pd.DataFrame()
+    for i,row in genes.iterrows():
+       
+        primernum = int(np.floor(i/6))
+        # get fwd and rev primer.
+        thefwdprimer = fwdprimes[primernum]
+        therevprimer = revprimes[primernum]
+        # mutate the sequence
+        tempdf =  mut_seq_random(row['geneseq'], thefwdprimer, therevprimer, nseqs)
+        # set which gene the group of sequences comes from.
+        tempdf['gene'] = row['name']
+        #we will build up the final dataframe of mutated sequences from the individual
+        #dataframes of sequences. 
+        allseqs = pd.concat([allseqs,tempdf],axis=0)
+        
+    return allseqs
+
+
+def check_mutation_rate(genes, sequences, buffer=10):
+    """
+    Check mutation rate of generated sequences.
+    
+    Parameters
+    ----------
+    genes : Pandas.DataFrame
+        DataFrame of gene names and sequences
+    sequences : Pandas.DataFrame
+        DataFrame of mutated sequences for each gene
+    buffer : int
+        Number of bases as buffer when checking non-primer and non-wildtype bases
+    """
+    
+    for i,row in genes.iterrows():
+        bad_rates = 0
+        
+        gene = row['name']
+        #dnaE gene is bugged out right now, just skip it for the moment.
+        if gene == 'dnaE':
+            continue
+
+        # get sequences from target gene
+        partialdf = sequences.loc[sequences['gene'] == gene,['seq']]
+
+        # Assign test count
+        partialdf['ct'] = 1
+        partialdf = partialdf[['ct','seq']]
+        partialdf = partialdf.reset_index(drop=True)
+        
+        # Compute mutation rate
+        mut = profile_mut.main(partialdf)
+
+        #Check mutation rate of non-primer and non-wildtype bases
+        relevantmuts = mut.loc[buffer + 20:179-buffer*2,'mut']
+
+        # Check for positions with abnormal mutation rate
+        if (relevantmuts > .14).any() or (relevantmuts < .07).any():
+            print('Bad mutation rate for gene {}.'.format(gene))
+            
+        # Check for repeated sequences
+        if len(partialdf['seq'].index) != len(partialdf['seq'].unique()):
+            print('Repeated sequences for gene {}.'.format(gene))
+            
+        # Check frequencies of each base in the sequence range
+        freqmat = profile_freq.main(partialdf)
+        relevantfreq = freqmat.loc[20 + buffer:179-buffer*2,:]
+        
+        # Check mutation rates of all bases
+        freqmin = relevantfreq[['freq_A','freq_C','freq_G','freq_T']].min(axis=1)
+        relevantfreqmat = np.array(relevantfreq[['freq_A','freq_C','freq_G','freq_T']])
+        if (freqmin < .02).any():
+            print("Base with low mutation rate for gene {} ".format(gene))
