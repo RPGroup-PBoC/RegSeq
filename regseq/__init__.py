5004e502f587a00a573f0eef86ced02bb88100a3
tomroesch <troeschi@caltech.edu>
2020-05-10 11:13:34 -0700

    modified init

--- regseq/__init__.py
+++ regseq/__init__.py
@@ -1,6 +1,6 @@
 from .utils import *
-from .plot_informationfootprint import *
 from .create_key import *
 from .seq_utils import *
 from .match_data import *
 from .inference import *
+from .information_footprint import *
